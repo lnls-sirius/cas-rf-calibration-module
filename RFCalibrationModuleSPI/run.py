aaefb4f9e955dc56c14dba28e7c51bed64723f30
carneirofc <claudiofcarneiro@hotmail.com>
2019-07-22 12:27:42 -0300

    SPI: Config SPI pins run.py

--- RFCalibrationModuleSPI/run.py
+++ RFCalibrationModuleSPI/run.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python
 import argparse
 import logging
+import os
 
 if __name__ == '__main__':
+
+    os.system('config-pin P9_17 spi_cs')         # CS
+    os.system('config-pin P9_21 spi')            # DO
+    os.system('config-pin P9_18 spi')            # DI
+    os.system('config-pin P9_22 spi_sclk')       # CLK
+
+    os.system('config-pin P9_14 out')            # Relay
+
+    os.system('config-pin P9_24 out')            # ADC 1
+    os.system('config-pin P9_26 out')            # ADC 2
+
     parser = argparse.ArgumentParser(description='RF calibration module SPI interface',
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--unix-socket-path', dest='unix_socket_path', help='UNIX socket address.', default='./unix-socket')
