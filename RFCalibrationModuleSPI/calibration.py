707343ddc8d8d009c94b8631e6a065442f173834
carneirofc <claudiofcarneiro@hotmail.com>
2019-09-17 17:08:23 -0300

    SPI: Calibration module

--- RFCalibrationModuleSPI/calibration.py
+++ RFCalibrationModuleSPI/calibration.py
@@ -11,16 +11,20 @@ class RF_Calibration_Module():
     def __init__(self):
         self.ADC1 = converters.ADC()
         self.ADC2 = converters.ADC2()
+        self.ADC1_values = [0.0] * 8
+        self.ADC2_values = [0.0] * 8
 
     def read(self):
-        ADC1_values = [0.0] * 8
-        ADC2_values = [0.0] * 8
+        for i in range(8):
+            self.ADC1_values[i] = 0
+            self.ADC2_values[i] = 0
 
         for i in range(10):
             ADC1_readings = self.ADC1.read(range(8))
             ADC2_readings = self.ADC2.read(range(8))
-            for j in range(8):
-                ADC1_values[j] += ADC1_readings[j]
-                ADC2_values[j] += ADC2_readings[j]
 
-        return str([1] + ADC1_values + ADC2_values)
+            for j in range(8):
+                self.ADC1_values[j] += ADC1_readings[j]
+                self.ADC2_values[j] += ADC2_readings[j]
+
+        return str([1] + self.ADC1_values + self.ADC2_values)
